bee92078ae1c83c7e63768e165ba092bd0669033
Qingtian <vfishv@gmail.com>
2019-11-08 16:37:00 +0800

    androidx

--- app/src/main/java/com/fivehundredpx/blurdemo/MainActivity.java
+++ app/src/main/java/com/fivehundredpx/blurdemo/MainActivity.java
@@ -5,7 +5,7 @@ import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.OvershootInterpolator;
 import android.widget.ImageView;
